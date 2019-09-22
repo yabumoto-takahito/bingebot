965cdafd2d5d3249c8bdb2bda06b409eee5bd3e7
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-22 15:20:19 +0900

    Initial Commit

---
+++ bingebot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bingebot
+//
+//  Created by 薮本崇仁 on 2019/09/22.
+//  Copyright © 2019 薮本崇仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
